06221870c66fa0d069ae57e5e2a51ef7db55ac50
Aldwin <aldwin828@gmail.com>
2021-07-30 21:53:29 -0400

    JS Axios

---
+++ app.js
@@ -0,0 +1,41 @@
+                            // Generate random activity
+let bttn = document.getElementById('bttn');
+
+function getActivity(){
+axios.get("http://www.boredapi.com/api/activity/").then(successActivity).catch(failedActivity);
+}
+
+function successActivity(response){
+    console.log(response);
+    document.getElementById('randomActivity').innerText = response.data.activity;
+}
+
+function failedActivity(err){
+    console.log(err);
+}
+
+bttn.addEventListener('click', getActivity)
+
+
+                            // Generate activity based on participantslet 
+button = document.getElementById('button');
+function getActivityByParticipants(){
+    axios.get("http://www.boredapi.com/api/activity/",
+    {
+        params: {
+            participants: numberOf.value
+        }
+    }).then(showActivity).catch(failedToShow);
+}
+
+function showActivity(success){
+    console.log(success);
+    document.getElementById('activity').innerText = success.data.activity;
+}
+
+function failedToShow(error){
+    console.log(error);
+    document.getElementById('activity').innerText = "Please try again!";
+}
+
+button.addEventListener('click', getActivityByParticipants)
